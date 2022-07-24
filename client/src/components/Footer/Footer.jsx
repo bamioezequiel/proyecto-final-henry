fc45cb215265903507cf042e9f7aa32c76d87547
tobiaszavallo <tobi-zavallo@hotmail.com>
2022-07-24 18:12:45 -0300

    agregado de iconos

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./Footer.module.css";
-// import iconoFacebook from "../../assets/img/facebook.png";
-// import iconoYoutube from "../../assets/img/youtube.png";
-// import iconoInstagram from "../../assets/img/instagrammm.png";
+import iconoFacebook from "../../assets/img/facebook.png";
+import iconoYoutube from "../../assets/img/youtube.png";
+import iconoInstagram from "../../assets/img/instagrammm.png";
 // import "bootstrap/dist/css/bootstrap.min.css";
  
 export default function Footer() {
@@ -21,9 +21,9 @@ export default function Footer() {
             </div>
             <div className={styles.divSeguinos}>
                 <h2 className={styles.Footer_h2}>Seguinos!</h2>
-                <a href="https://www.facebook.com/" className={styles.a}><img src={'iconoFacebook'} alt="img"/></a>
-                <a href="https://www.youtube.com/" className={styles.a}><img src={'iconoYoutube'} alt="img"/></a>
-                <a href="https://www.instagram.com/" className={styles.a}><img src={'iconoInstagram'} alt="img"/></a>
+                <a href="https://www.facebook.com/" className={styles.a}><img src={iconoFacebook} alt="img"/></a>
+                <a href="https://www.youtube.com/" className={styles.a}><img src={iconoYoutube} alt="img"/></a>
+                <a href="https://www.instagram.com/" className={styles.a}><img src={iconoInstagram} alt="img"/></a>
             </div>
         </div>
     )
